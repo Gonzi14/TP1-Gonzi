db33d7e4931c73a2f1f546ad9386dbcb42a5e442
Gonzi14 <gonzaloandrade142@gmail.com>
2021-04-24 19:59:09 -0300

    termine el 7.6

--- ej7.6.py
+++ ej7.6.py
@@ -1,5 +1,4 @@
 diasquepasaron = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
-mesennumeritos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 
 def CheQueRapidoPasaElTiempo(mes, dia, diasquepasaron):
@@ -7,6 +6,7 @@ def CheQueRapidoPasaElTiempo(mes, dia, diasquepasaron):
     for i in range(1, 12):
         if i == mes:
             totalidaddediasquepasaronhastahoy = diasquepasaron[i - 1] + dia
+            #lo que estoy haciendo es sumar los dias que ya pasaron, sabiendo el mes, con el dia que me dijo el chico
     return totalidaddediasquepasaronhastahoy
 
 
