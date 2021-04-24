d4472ecfe4d3404f3728f94615331f07ba5bff50
Gonzi14 <gonzaloandrade142@gmail.com>
2021-04-24 18:52:41 -0300

    Ahi mando el 7.6 porque lo hice a la velocidad de la luz

--- ej7.6.py
+++ ej7.6.py
@@ -1,2 +1,16 @@
 diasquepasaron = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
 mesennumeritos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+
+def CheQueRapidoPasaElTiempo(mes, dia, diasquepasaron):
+    totalidaddediasquepasaronhastahoy = 0
+    for i in range(1, 12):
+        if i == mes:
+            totalidaddediasquepasaronhastahoy = diasquepasaron[i - 1] + dia
+    return totalidaddediasquepasaronhastahoy
+
+
+if 0 == 0:
+    dia = int(input("che me pasas el dia en el que estamos:  "))
+    mes = int(input("estoy re perdido man sorry, pero me decis el mes:  "))
+    print(CheQueRapidoPasaElTiempo(mes, dia, diasquepasaron))
