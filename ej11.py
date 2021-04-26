d3fc893f744b33ddd2157310b39429b620351354
Gonzi14 <gonzaloandrade142@gmail.com>
2021-04-25 21:53:23 -0300

    agrego comentarios al ej 11

--- ej11.py
+++ ej11.py
@@ -1,9 +1,4 @@
-"""Escribir una función que dada la cantidad de ejercicios de un examen, y el porcentaje
-necesario de ejercicios bien resueltos necesario para aprobar dicho examen, revise un grupo
-de exámenes. Para ello, en cada paso debe preguntar la cantidad de ejercicios resueltos por
-el alumno, indicando con un valor centinela que no hay más exámenes a revisar. Debe
-mostrar por pantalla el porcentaje correspondiente a la cantidad de ejercicios resueltos
-respecto a la cantidad de ejercicios del examen y una leyenda que indique si aprobó o no."""
+
 
 def HacerloTodoOtraVez(SioNo):
     #aca me fijo si el usuario quiere usar otra vez el programa, osea tiene otro examen que corregir
