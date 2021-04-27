acdc5fd7420f883eb3b5cdb7d2ed79dd87714318
Gonzi14 <gonzaloandrade142@gmail.com>
2021-04-27 20:59:48 -0300

    mando el 8

---
+++ ej8.py
@@ -0,0 +1,17 @@
+diccionario = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"),
+           (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+
+
+def Roma(numero,diccionarioromano):
+    añoromano = ""
+
+
+    while numero > 0:
+        for lsNumeros, lsLetras in diccionario:
+            while numero >= lsNumeros:
+                añoromano = añoromano + lsLetras
+                numero = numero - lsNumeros
+        return añoromano
+
+
+print(Roma(int(input("escribime el año que queres pasar: ")),diccionario))
