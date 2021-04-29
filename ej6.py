ed4cf71ce102d78e8874fc5bc4670f47638ef0f5
Gonzi14 <gonzaloandrade142@gmail.com>
2021-04-28 23:12:31 -0300

    R.I.P Prrrrrrrrrrrroducto

--- ej6.py
+++ ej6.py
@@ -4,22 +4,24 @@ numerito3 = int(input("dame el Numero 3: "))
 numerito4 = int(input("dame el Numero 4: "))
  # profe porfa no me desapruebes :(
 
-def prrrrrrrrrrrroducto(numerito1, numerito2, numerito3, numerito4):
-    mayorPrrrrrrrrrrrroducto = 0
+def producto(numerito1, numerito2, numerito3, numerito4):
+    mayorProducto = -10000000
     for i in (numerito2, numerito3, numerito4):
-        prrrrrrrrrrrroducto = numerito1 * i
-        if (prrrrrrrrrrrroducto > mayorPrrrrrrrrrrrroducto):
-            mayorPrrrrrrrrrrrroducto = prrrrrrrrrrrroducto
+        producto = numerito1 * i
+        if (producto > mayorProducto):
+            mayorProducto = producto
+
     for i in (numerito3, numerito4):
-        prrrrrrrrrrrroducto = numerito2 * i
-        if (prrrrrrrrrrrroducto > mayorPrrrrrrrrrrrroducto):
-            mayorPrrrrrrrrrrrroducto = prrrrrrrrrrrroducto
+        producto = numerito2 * i
+        if (producto > mayorProducto):
+            mayorProducto = producto
+
             """ aca lo q digo es que si el promedio que hiciste recien es mayor que lo que antes  considerabas mejor 
             promedio entonces este es el nuevo mayor jaja salu2 """
-    prrrrrrrrrrrroducto = numerito3 * numerito4
-    if (prrrrrrrrrrrroducto > mayorPrrrrrrrrrrrroducto):
-        mayorPrrrrrrrrrrrroducto = prrrrrrrrrrrroducto
-        return f"el mayor producto es: {mayorPrrrrrrrrrrrroducto}"
+    producto = numerito3 * numerito4
+    if (producto > mayorProducto):
+        mayorProducto = producto
+    return f"el mayor producto es: {mayorProducto}"
 
 
-print(prrrrrrrrrrrroducto(numerito1, numerito2, numerito3, numerito4))
+print(producto(numerito1, numerito2, numerito3, numerito4))
